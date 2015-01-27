306ed89fdc9c82521b1a6fa3679ca926c6d3ee28
pattyboi <patpat1325179@gmail.com>
2015-01-27 17:20:23 -0500

    derp

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -55,11 +55,9 @@ enum pageflags {
 	PG_readahead,		
 	__NR_PAGEFLAGS,
 
-	PG_readahead,		/* page in a readahead window */
 #ifdef CONFIG_KSM_CHECK_PAGE
 	PG_ksm_scan0,		/* page has been scanned by even KSM cycle */
 #endif
-	__NR_PAGEFLAGS,
 
 #ifdef CONFIG_KSM_CHECK_PAGE
 	/* page has been scanned by odd KSM cycle */
